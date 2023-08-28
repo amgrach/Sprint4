fc541f5dc93edaa446f3df83a70f1f10fe9dd752
amgrach <amgrach@gmail.com>
2023-08-29 00:26:57 +0300

    add timeouts

--- src/main/java/org/example/page/MainPage.java
+++ src/main/java/org/example/page/MainPage.java
@@ -2,6 +2,10 @@ package org.example.page;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.example.config.AppConfig.APP_URL;
 import static org.example.config.AppConfig.IS_THIS_A_BOTTOM_ORDER_TEST;
@@ -23,11 +27,14 @@ public class MainPage {
     }
 
     public void clickOnQuestionId(String questionNumber) {
+
         driver.findElement(By.id("accordion__heading-" + questionNumber)).click();
 
     }
 
     public String getActualAnswer(String answerNumber) {
+        new WebDriverWait(driver, Duration.ofSeconds(3))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("accordion__panel-" + answerNumber)));
         return driver.findElement(By.id("accordion__panel-" + answerNumber)).getText();
 
     }
